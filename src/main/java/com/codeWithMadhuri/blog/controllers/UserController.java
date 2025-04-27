a5208420480b85026a3e0b9c32911a725d4a9fdd
madhuri <guravmadhuri93@gmail.com>
2025-04-27 20:20:27 +0530

    logger added by madhuri

--- src/main/java/com/codeWithMadhuri/blog/controllers/UserController.java
+++ src/main/java/com/codeWithMadhuri/blog/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.codeWithMadhuri.blog.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,16 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codeWithMadhuri.blog.payloads.UserDto;
 import com.codeWithMadhuri.blog.services.UserService;
 
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+	private static Logger logger = LoggerFactory.getLogger(UserController.class);
 @Autowired
 private UserService userService;
 @PostMapping("/")
 public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
-	
+	logger.info("create user method started");
 	UserDto createUserDto=this.userService.createUser(userDto);
-	
+	logger.info("create user method ended");
 	return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
 	
 	
